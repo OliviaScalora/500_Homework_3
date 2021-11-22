c19ef81c5575ccd937b00878c8ff2d6c4c21b5fd
OliviaScalora <oscalora@design.upenn.edu>
2021-11-22 14:54:20 -0500

    Got through Step 2

--- Homework3_Script.R
+++ Homework3_Script.R
@@ -39,6 +39,8 @@ library(sp)
 library(spatialreg)
 library(kableExtra)
 library(knitr)
+#install.packages('DT')
+library(DT)
 
 
 mydata <- read.csv("https://raw.githubusercontent.com/OliviaScalora/500_Homework_3/main/Logistic Regression Data.csv")
@@ -90,87 +92,187 @@ prop.table(DRINKING_D.tab)
 Cross_Fatal<-CrossTable(mydata$FATAL_OR_M, mydata$DRINKING_D, prop.r=FALSE, prop.t=FALSE, prop.chisq=FALSE, chisq=TRUE)
 Cross_Fatal_tbl<-
   cbind(as.data.frame(Cross_Fatal$t)%>%filter(x==1)%>%rename(N=Freq),
-        as.data.frame(Cross_Fatal[["prop.tbl"]])%>%filter(x==1)%>%rename('%'=Freq,x1=x,y1=y))%>%
+        as.data.frame(Cross_Fatal[["prop.tbl"]])%>%filter(x==1)%>%rename('percentage'=Freq,x1=x,y1=y))%>%
   dplyr::select(-x1,-y1)%>%
   mutate(Variable='FATAL_OR_M')
+         
 
 #.ii
 Cross_Over<- CrossTable(mydata$OVERTURNED, mydata$DRINKING_D, prop.r=FALSE, prop.t=FALSE, prop.chisq=FALSE, chisq=TRUE)
 Cross_Over_tbl<-
   cbind(as.data.frame(Cross_Over$t)%>%filter(x==1)%>%rename(N=Freq),
-      as.data.frame(Cross_Over[["prop.tbl"]])%>%filter(x==1)%>%rename('%'=Freq,x1=x,y1=y))%>%
+      as.data.frame(Cross_Over[["prop.tbl"]])%>%filter(x==1)%>%rename('percentage'=Freq,x1=x,y1=y))%>%
   dplyr::select(-x1,-y1)%>%
   mutate(Variable='OVERTURNED')
 
 Cross_Cell<-CrossTable(mydata$CELL_PHONE, mydata$DRINKING_D, prop.r=FALSE, prop.t=FALSE, prop.chisq=FALSE, chisq=TRUE)
 Cross_Cell_tbl<-
   cbind(as.data.frame(Cross_Cell$t)%>%filter(x==1)%>%rename(N=Freq),
-        as.data.frame(Cross_Cell[["prop.tbl"]])%>%filter(x==1)%>%rename('%'=Freq,x1=x,y1=y))%>%
+        as.data.frame(Cross_Cell[["prop.tbl"]])%>%filter(x==1)%>%rename('percentage'=Freq,x1=x,y1=y))%>%
   dplyr::select(-x1,-y1)%>%
   mutate(Variable='CELL_PHONE')
 
 Cross_Speed<-CrossTable(mydata$SPEEDING, mydata$DRINKING_D, prop.r=FALSE, prop.t=FALSE, prop.chisq=FALSE, chisq=TRUE)
 Cross_Speed_tbl<-
   cbind(as.data.frame(Cross_Speed$t)%>%filter(x==1)%>%rename(N=Freq),
-        as.data.frame(Cross_Speed[["prop.tbl"]])%>%filter(x==1)%>%rename('%'=Freq,x1=x,y1=y))%>%
+        as.data.frame(Cross_Speed[["prop.tbl"]])%>%filter(x==1)%>%rename('percentage'=Freq,x1=x,y1=y))%>%
   dplyr::select(-x1,-y1)%>%
   mutate(Variable='SPEEDING')
 
 Cross_Agg<-CrossTable(mydata$AGGRESSIVE, mydata$DRINKING_D, prop.r=FALSE, prop.t=FALSE, prop.chisq=FALSE, chisq=TRUE)
 Cross_Agg_tbl<-
   cbind(as.data.frame(Cross_Agg$t)%>%filter(x==1)%>%rename(N=Freq),
-        as.data.frame(Cross_Agg[["prop.tbl"]])%>%filter(x==1)%>%rename('%'=Freq,x1=x,y1=y))%>%
+        as.data.frame(Cross_Agg[["prop.tbl"]])%>%filter(x==1)%>%rename('percentage'=Freq,x1=x,y1=y))%>%
   dplyr::select(-x1,-y1)%>%
   mutate(Variable='AGGRESSIVE')
 
 Cross_1617<-CrossTable(mydata$DRIVER1617, mydata$DRINKING_D, prop.r=FALSE, prop.t=FALSE, prop.chisq=FALSE, chisq=TRUE)
 Cross_1617_tbl<-
   cbind(as.data.frame(Cross_1617$t)%>%filter(x==1)%>%rename(N=Freq),
-        as.data.frame(Cross_1617[["prop.tbl"]])%>%filter(x==1)%>%rename('%'=Freq,x1=x,y1=y))%>%
+        as.data.frame(Cross_1617[["prop.tbl"]])%>%filter(x==1)%>%rename('percentage'=Freq,x1=x,y1=y))%>%
           dplyr::select(-x1,-y1)%>%
           mutate(Variable='DRIVER1617')
 
 Cross_65P<-CrossTable(mydata$DRIVER65PLUS, mydata$DRINKING_D, prop.r=FALSE, prop.t=FALSE, prop.chisq=FALSE, chisq=TRUE)
 Cross_65P_tbl<-
   cbind(as.data.frame(Cross_65P$t)%>%filter(x==1)%>%rename(N=Freq),
-        as.data.frame(Cross_65P[["prop.tbl"]])%>%filter(x==1)%>%rename('%'=Freq,x1=x,y1=y))%>%
+        as.data.frame(Cross_65P[["prop.tbl"]])%>%filter(x==1)%>%rename('percentage'=Freq,x1=x,y1=y))%>%
   dplyr::select(-x1,-y1)%>%
   mutate(Variable='DRIVER65PLUS')
 
-#having a hard time spreading the table to have the same columns as Eugene's example in the homework instructions, 
-#but all of the values we need are here, may just make a table manually
+# Putting table together - there might be a simpler way of doing this but this ends up working
 Cross_Joined<-rbind(Cross_Fatal_tbl,Cross_Over_tbl,Cross_Cell_tbl,Cross_Speed_tbl,Cross_Agg_tbl,Cross_1617_tbl,Cross_65P_tbl)
 
-CrossTable <-
-  Cross_Joined %>%
-  spread(x, Variable)
-  mutate(bind_cols(data.frame(Cost.50 = c(
-    "639000",
-    "7636335",
-    "12091741",
-    "1980000"))),
-    Cost.40 = case_when(Variable == "True_Negative"  ~ (Count * 0.1) * 10000,
-                        Variable == "True_Positive"  ~ (Count *.75 *10000) + (Count *.25 * 42727),
-                        Variable == "False_Negative" ~ Count * 42727,
-                        Variable == "False_Positive" ~ Count * 10000),
-    bind_cols(data.frame(Social.Benefit = c(
-      "Individual re-enters society and becomes a productive member of local economy without government program assistance",
-      "Individual receives job training and career support and has a 75% chance of not recidivating. Community gains a productive member of local economy",
-      "NA",
-      "Individual receives job training and career support and has a 100% chance of not recidivating. Community gains a productive member of local economy"))),
-    bind_cols(data.frame(Description = c(
-      "Individual is predicted to not recidivate and does not in reality.",
-      "Individual is predicted to recidivate and does in reality.",
-      "Individual is predicted to not recidivate but does in reality.",
-      "Individual is predicted to recidivate but does not in reality."))))
+CrossTablex<- Cross_Joined %>%spread(x, Variable)%>%filter(y==0)%>%rename(N_1=N,'percentage_1' = 'percentage')
+CrossTabley<- Cross_Joined %>%spread(x, Variable)%>%filter(y==1)%>%
+  dplyr::select(-y,-'1')%>%
+  cbind(CrossTablex)%>%dplyr::select(-y)%>%rename(Variable = '1')
+CrossTable<-CrossTabley%>%mutate(Total_N = N+N_1,
+         '%' = round((percentage *100),2),
+         '%_1' = round((percentage_1 *100),2))%>% dplyr::select(-percentage, -percentage_1)%>%
+  arrange(factor(Variable, levels = c('FATAL_OR_M', 'OVERTURNED', 
+                                      'CELL_PHONE', 'SPEEDING', 
+                                      'AGGRESSIVE', 'DRIVER1617', 
+                                      'DRIVER65PLUS')))%>%
+  bind_cols(data.frame(Description = c(
+  "Crash resulted in fatality or major injury",
+  "Crash involved an overturned vehicle",
+  "Driver was using cell phone",
+  "Crash involved speeding vat",
+  "Crash involved aggressive driving",
+  "Crash involved at least one driver who was 16 or 17 years old",
+  "Crash involved at least one driver who was at least 65 years old")))%>%
+  bind_cols(data.frame('X2 p.value' = c(
+    Cross_Fatal[["chisq"]][["p.value"]],
+    Cross_Over[["chisq"]][["p.value"]],
+    Cross_Cell[["chisq"]][["p.value"]],
+    Cross_Speed[["chisq"]][["p.value"]],
+    Cross_Agg[["chisq"]][["p.value"]],
+    Cross_1617[["chisq"]][["p.value"]],
+    Cross_65P[["chisq"]][["p.value"]])))
 
+#iii.
+#Output Table
+CrossTable[, c(3,7,1,5,2,6,4,8)]%>%
+  kable(col.names = c("Variable", "","N","%","N","%","N", "X2 P.value"),
+                                     align = c('r','l','c','c','c','c','c','c'))%>%
+  kable_material()%>%
+  add_header_above(c("","","No Alcohol Involved\n(Drinkind_D = 0)" = 2, "Alcohol Involved\n(Drinkind_D = 1)" = 2,"Total", ""),
+                                                align = c('r','c'))%>%
+  column_spec(1,bold = TRUE)%>%
+  column_spec(5,border_left = TRUE)%>%
+  column_spec(c(3:4),background = '#f1f1f1')%>%
+  column_spec(c(5:6),background = '#e1e1e1')
+  
+#iv.
+#Chi-Square test 
+#H0 - the proportion of fatalities for crashes that involve drunk drivers is the same as the proportion
+#of fatalities for crashes that dont involve drunk drivers
+#Ha - the proportion of fatalities for crashes that involve drunk drivers is different than 
+# the proportion of fatalities for crashes that don't involve drunk drivers.
+
+#added X2 pvalue column to table above ^
+
+#2.c
+
+#i.
 #Means by group
-tapply(mydata$PCTBACHMOR, mydata$DRINKING_D, mean) 
-tapply(mydata$MEDHHINC, mydata$DRINKING_D, mean) 
+meanbach<-as.data.frame(t(tapply(mydata$PCTBACHMOR, mydata$DRINKING_D, mean)))%>%mutate(Variable= 'PCTBACHMOR')%>%
+  rename('mean.0'='0', 'mean.1'='1')
 
-#Independent samples t-test
-t.test(mydata$PCTBACHMOR~mydata$DRINKING_D)
-t.test(mydata$MEDHHINC~mydata$DRINKING_D)
+#        0        1 
+# 16.56986 16.61173 
+
+meanHHinc<-as.data.frame(t(tapply(mydata$MEDHHINC, mydata$DRINKING_D, mean)))%>%mutate(Variable= 'MEDHHINC')%>%
+  rename('mean.0'='0', 'mean.1'='1')
+
+#        0        1 
+# 31483.05 31998.75 
+
+#SD by group
+sdbach<-as.data.frame(t(tapply(mydata$PCTBACHMOR, mydata$DRINKING_D, sd)))%>%mutate(Variable= 'PCTBACHMOR')%>%
+  rename('sd.0'='0', 'sd.1'='1')
+
+#        0        1 
+# 18.21426 18.72091
+
+sdHHinc<-as.data.frame(t(tapply(mydata$MEDHHINC, mydata$DRINKING_D, sd)))%>%mutate(Variable= 'MEDHHINC')%>%
+  rename('sd.0'='0', 'sd.1'='1')
+
+sdHHinc<-sdHHinc%>%spread(sd,x)
+#       0       1 
+# 16930.1 17810.5
+
+#save pvalues for table (step iii). 
+
+ttest_pvalues<-as.data.frame(c((t.test(mydata$PCTBACHMOR~mydata$DRINKING_D)$p.value),
+                               (t.test(mydata$MEDHHINC~mydata$DRINKING_D)$p.value)))
+
+#ii.
+#Mean and SD table
+MeanSD_tbl<- merge(x=(merge(x=meanHHinc, y=sdHHinc, all= TRUE)),
+                   y=(merge(x=meanbach, y=sdbach,all=TRUE)),
+                   all=TRUE)%>%arrange(desc(Variable))%>%
+  bind_cols(data.frame(Description = c(
+    "% with a bachelor's degree or more",
+    "Median household income")))%>%
+  bind_cols(ttest_pvalues)
+
+#output for markdown
+MeanSD_tbl[, c(1,6,2,4,3,5,7)]%>%
+  kable(col.names = c("Variable","","Mean","SD","Mean","SD","t-test p-value"),
+                                     align = c('r','l','c','c','c','c','c'))%>%
+  kable_material()%>%
+  add_header_above(c("","","No Alcohol Involved\n(Drinkind_D = 0)" = 2,
+                     "Alcohol Involved\n(Drinkind_D = 1)" = 2,""),
+                   align = c('r','c'))%>%
+  column_spec(1,bold = TRUE)%>%
+  column_spec(5,border_left = TRUE)%>%
+  column_spec(c(3:4),background = '#f1f1f1')%>%
+  column_spec(c(5:6),background = '#e1e1e1')
+
+#iii.
+#added t-test p-value to table above^^
+
+#2.d Using the instructions from Assignment 1, examine the Pearson correlations
+#between all the predictors, (both binary and continuous). Is there evidence
+# of severe multicollinearity here?
+
+
+pred_var <- mydata%>%dplyr::select(DRINKING_D, FATAL_OR_M, OVERTURNED, CELL_PHONE, SPEEDING,
+                                   AGGRESSIVE,DRIVER1617,DRIVER65PLUS,PCTBACHMOR,MEDHHINC)
+pcorr <- cor(pred_var, method="pearson")
+
+
+#correlation matrix for markdown
+pcorr%>%kable()%>%kable_material()%>%
+  column_spec(1,bold = TRUE)
+
+#Observe that there isnt severe multicollinearity (i.e., no correlations where
+# r>.8 or r<-.8), so we can include all four predictors in the regression.
+
+
+
 
 #Logistic Regression
 mylogit <- glm(DRINKING_D ~ FATAL_OR_M + OVERTURNED + CELL_PHONE + SPEEDING + AGGRESSIVE + DRIVER1617 + DRIVER65PLUS + PCTBACHMOR + MEDHHINC, data=mydata, family = "binomial")
